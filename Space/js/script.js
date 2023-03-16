615f2b9da361f6daaad230ea55f26df1d9120f68
Ivandefender <61967395+Ivandefender@users.noreply.github.com>
2023-03-16 15:44:22 +0200

    Add files via upload

--- Space/js/script.js
+++ Space/js/script.js
@@ -8,6 +8,66 @@ link.addEventListener('click', linkClick);
 function linkClick(event) {
     event.preventDefault();
     //backBlur.style.backdropfilter = backBlur.style.backdropfilter === 'transparent' ? 'blur(40.7742px)' : 'transparent';
+    backColor.style.display = backColor.style.display === 'none' ? 'block' : 'none';
     backColor.style.background = backColor.style.background === 'transparent' ? 'rgba(0, 0, 0, 0.9)' : 'transparent';
     listItems.style.display = listItems.style.display === 'none' ? 'block' : 'none';
 }
+
+
+$(function() {
+    $('.crew__slider').slick({
+        arrows: false,
+        dots: true,
+        vertical: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
+    })
+
+    // $('.menu__btn').on('click', function(){
+    //     $('.menu__list').toggleClass('menu__list--active');
+    // })
+
+    $('.slider-slick').slick({
+        arrows: false,
+        dots: true,
+        vertical: false,
+        
+    })
+
+    $('.crew__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1140,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 840,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 580,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    })
+});
+
